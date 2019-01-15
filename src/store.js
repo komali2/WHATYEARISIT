34105919f5c83faa2cbf613cf74fdcc88b37176b
Caleb Rogers <rogersjcaleb@gmail.com>
2019-01-14 20:47:21 -0800

    Store configuration attempt

--- src/store.js
+++ src/store.js
@@ -1,16 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
-})
+    state: {
+        session: null,
+        photos: [],
+    },
+    mutations: {
+        addPhoto(state, photo){
+            state.photos.push(photo);
+        },
+        addPhotos(state, photos){
+            photos.forEach((photo)=>{
+                state.commit('addPhoto', photo);
+            });
+        },
+    },
+    actions: {
+        fetchPhotos(context){
+            return new Promise((resolve)=>{
+                resolve([
+                    {
+                        id: 1,
+                        url: 'bob.com',
+                    },
+                    {
+                        id: 2,
+                        url: 'joe.com',
+                    },
+                    {
+                        id: 3,
+                        url: 'sally.com',
+                    },
+                    {
+                        id: 4,
+                        url: 'yes.com',
+                    },
+                ]);
+            });
+        },
+        savePhotos({ commit, dispatch, }){
+            dispatch('fetchPhotos').then((photos)=>{
+                commit('addPhotos', photos);
+            });
+        },
+    },
+});
